9ff93168c54e5f6c3ee3c045d20b426d0bb1d50a
Goodya <1799972130@qq.com>
2018-07-30 12:34:24 +0800

    ...

---
+++ vue-cli/src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>这是我的首页 </h2>
+
+        <h3> {{$store.state.count}}</h3>
+
+    </div>
+</template>
+
+<script>
+    import store from '@/vuex/store'
+    export default {
+        store
+    }
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-cli/src/components/Test2.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        test2
+
+        {{$route.params.username}}
+        {{$route.params.userid}}
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
